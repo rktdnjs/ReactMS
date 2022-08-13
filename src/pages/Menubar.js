f0832a781ed4392c91d20a9a685998f8fcd48fac
rktdnjs <rktdnjs@naver.com>
2022-08-13 14:58:57 +0900

    4-13 useNavigate

--- src/pages/Menubar.js
+++ src/pages/Menubar.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+// Outlet을 사용하면 라우터설정이 된 곳에서 하위 컴포넌트의 항목도 보여준다.
+// 중첩경로로 설정된 컴포넌트 중 하위 컴포넌트들.
+import { useNavigate } from 'react-router-dom';
 
 const Menubar = () => {
+    const navigate = useNavigate();
+    const goHome = () => {
+        navigate("/");
+    };
+
     return (
         <div>
             <ul>
                 <li><Link to="/home">Home</Link></li>
                 <li><Link to="/movies">Movies</Link></li>
             </ul>
+            <button onClick={goHome} type='button'>홈으로 돌아가기</button>
             <Outlet/>
         </div>
     );
